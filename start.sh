660c934208f16e36107589f5df4b2c04d53536ab
Dominique Barton <dbarton@confirm.ch>
2015-12-19 19:40:39 +0100

    download and unpack in memory

--- start.sh
+++ start.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Download new utorrent server.
 echo 'Downloading new utorrent server...'
-wget -q /tmp/utserver.tar.gz http://download.ap.bittorrent.com/track/beta/endpoint/utserver/os/linux-x64-debian-7-0 \
-    && rm -rf * \
-    && tar xzf /tmp/utserver.tar.gz --strip-components 1
+rm -rf * && wget -qO - http://download.ap.bittorrent.com/track/beta/endpoint/utserver/os/linux-x64-debian-7-0 | tar xzf - --strip-components 1
 
-# Symlink webui.zip.
-echo
-echo 'Symlinking webui.zip to /settings...'
-ln -sf /utorrent/webui.zip /settings/webui.zip
 
-# Start utorrent.
+if [[ ! -e /settings/webui.zip ]]
+then
+    echo
+    echo 'Symlinking webui.zip to /settings...'
+    ln -s /utorrent/webui.zip /settings/webui.zip
+fi
+
 echo
 echo 'Starting utorrent server...'
 ./utserver -settingspath /settings -logfile /settings/utserver.log
